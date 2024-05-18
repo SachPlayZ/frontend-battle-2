564fc5388d9d695080f4f673bd43c3a07ccc1879
SachPlayZ <ssachinsingh99@gmail.com>
2024-05-18 17:28:06 +0530

    Added progress

---
+++ script.js
@@ -0,0 +1,7 @@
+// scripts.js
+
+document.getElementById('mobile-menu').addEventListener('click', function() {
+    const nav = document.querySelector('.nav-list');
+    nav.classList.toggle('showing');
+  });
+  
