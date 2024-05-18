dbd649f3ca69cbae4f00da9a827844c533cf30ba
SachPlayZ <ssachinsingh99@gmail.com>
2024-05-19 03:32:01 +0530

    Final few changes

--- scripts.js
+++ scripts.js
@@ -11,3 +11,26 @@ document.querySelectorAll('.dropdown > a').forEach(dropdown => {
         dropdownContent.classList.toggle('show');
     });
 });
+
+const slides = document.querySelectorAll('.carousel-slide');
+const prevButton = document.querySelector('.prev');
+const nextButton = document.querySelector('.next');
+let currentSlide = 0;
+
+function showSlide(index) {
+  if (index >= slides.length) {
+    currentSlide = 0;
+  } else if (index < 0) {
+    currentSlide = slides.length - 1;
+  } else {
+    currentSlide = index;
+  }
+  const offset = -currentSlide * 100;
+  document.querySelector('.carousel-content').style.transform = `translateX(${offset}%)`;
+}
+
+prevButton.addEventListener('click', () => showSlide(currentSlide - 1));
+nextButton.addEventListener('click', () => showSlide(currentSlide + 1));
+
+// Auto-play functionality
+setInterval(() => showSlide(currentSlide + 1), 5000); // Change slide every 5 seconds
